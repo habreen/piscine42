07a78f1d83706aeef8b885378097fbda8750f3ad
hi <86646149+habreen@users.noreply.github.com>
2025-09-01 22:36:39 +0800

    c02

---
+++ c02final/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshamudh <hshamudh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 20:10:25 by hshamudh          #+#    #+#             */
+/*   Updated: 2025/08/10 14:08:59 by hshamudh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			i++;
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/* int main (void)
+{
+	char	*str;
+
+	str = "dHhsh";
+	printf("%d", ft_str_is_lowercase(str));
+} */
---
+++ c02final/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshamudh <hshamudh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 20:10:25 by hshamudh          #+#    #+#             */
+/*   Updated: 2025/08/07 15:05:10 by hshamudh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'A' || str[i] > 'Z')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+/* #include <unistd.h>
+#include <stdio.h>
+
+int main (void)
+{
+	char	str[10] = "adas";
+	int		result;
+
+	result = ft_str_is_uppercase(str);
+	printf("%d", result);
+} */
